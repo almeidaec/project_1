1a1ead6a6f1b9c94b5bbb16dd471e61275de8494
Ellen de Almeida <ellencosta23@gmail.com>
2018-04-16 17:02:16 -0300

    Criando o repositorio :)

---
+++ project_1/__init__.py
@@ -0,0 +1,15 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from astropy.io import fits
+
+#Criando um master_bias
+
+def master_bias(pasta_bias):
+	im_bias = []
+	
+	
+	
+	
+
